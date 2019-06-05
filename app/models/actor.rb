5f69ac3e3f8e0b1213db4c6b117ee9448a57fe0f
y-amadatsu <y-amadatsu@ruby-dev.jp>
2019-06-05 19:17:30 +0900

    db:seedが動かなかったので修正

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,5 +1,5 @@
 class Actor < ApplicationRecord
-  has_many :movie_actors, -> {order('id')}
+  has_many :movie_actors, -> {order('id')}, inverse_of: :actor
   has_many :movies, through: :movie_actors
   accepts_nested_attributes_for :movie_actors
 end
--- app/models/movie_actor.rb
+++ app/models/movie_actor.rb
@@ -1,4 +1,4 @@
 class MovieActor < ApplicationRecord
-  belongs_to :actor
+  belongs_to :actor, inverse_of: :movie_actors
   belongs_to :movie
 end
